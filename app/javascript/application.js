461371288fb854b91b8dedde9255b63514331712
Dalton Huey <daltonjhuey@gmail.com>
2022-03-10 17:33:57 -0600

    build initial app with react and esbuild

---
+++ app/javascript/application.js
@@ -0,0 +1,6 @@
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers"
+import React from "react"
+import ReactDOM from "react-dom"
+import "./components/app.jsx"
