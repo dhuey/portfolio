0d92998eda7528646223806bcc7f7bc4223a1b14
Dalton Huey <daltonjhuey@gmail.com>
2022-03-10 19:57:42 -0600

    add videos to db

---
+++ db/migrate/20220311015655_create_videos.rb
@@ -0,0 +1,11 @@
+class CreateVideos < ActiveRecord::Migration[7.0]
+  def change
+    create_table :videos do |t|
+      t.string :title
+      t.string :youtube_link
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
