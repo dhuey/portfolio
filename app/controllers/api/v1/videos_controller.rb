672269e7184c5d73bba5b41e60ae39c389e0a533
Dalton Huey <daltonjhuey@gmail.com>
2022-03-16 11:37:38 -0500

    after submitting video form, redirect to videos index

--- app/controllers/api/v1/videos_controller.rb
+++ app/controllers/api/v1/videos_controller.rb
@@ -9,7 +9,7 @@ class Api::V1::VideosController < ApplicationController
     @video = Video.new(video_params)
 
     if @video.save
-      render json: @video, status: :created, location: api_v1_videos_url
+      render json: @video, status: :created
     else
       render json: @video.errors, status: :unprocessable_entity
     end
--- app/javascript/components/VideoForm.jsx
+++ app/javascript/components/VideoForm.jsx
@@ -1,30 +1,29 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export const VideoForm = () => {
   const [video, setVideo] = useState({title: "", youtube_link: "", description: ""});
   const [dbError, setDbError] = useState([]);
+  const navigate = useNavigate();
 
   const handleTitleChange = e => {
     setVideo({title: e.target.value, youtube_link: video.youtube_link, description: video.description});
-    console.log(video)
   }
 
   const handleLinkChange = e => {
     setVideo({title: video.title, youtube_link: e.target.value, description: video.description})
-    console.log(video)
   }
 
   const handleDescChange = e => {
     setVideo({title: video.title, youtube_link: video.youtube_link, description: e.target.value})
-    console.log(video)
   }
 
   const submitVideo = async () => {
     setDbError([]);
     try {
       const response = await axios.post('http://localhost:3000/api/v1/videos', video);
-      console.log(response.data);
+      navigate('/video');
     } catch (error) {
       console.log(error)
     };
