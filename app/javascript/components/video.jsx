eb6470bf039c85ba3fb0473d201007b88f8c5a6b
Dalton Huey <daltonjhuey@gmail.com>
2022-03-18 13:41:10 -0500

    style hero section with placeholder image

--- app/javascript/components/video.jsx
+++ app/javascript/components/video.jsx
@@ -25,7 +25,13 @@ export const Video = () => {
 
   return (
     <div>
-      <h1>dalton huey<br /><span className="h1-dash">&mdash;</span>videographer</h1>
+      <div className="hero-section">
+        <div>
+          <h1>dalton huey<br /><span className="h1-dash">&mdash;</span>videographer</h1>
+          <p>Big and small, I’m passionate about telling stories that matter. Perfectly content to be a writer, a director, a DP, or all of them at once.</p>
+        </div>
+        <img src="/dalton-profile-large.jpg" />
+      </div>
 
       {videos.map((videoProject) => {
         return <VideoProject key={videoProject.id} id={videoProject.id} title={videoProject.title} link={videoProject.youtube_link} description={videoProject.description} />
