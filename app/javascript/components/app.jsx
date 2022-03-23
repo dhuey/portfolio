288d8d9a10e48b8f0f1e5592cf6d577818095b47
Dalton Huey <daltonjhuey@gmail.com>
2022-03-23 11:58:08 -0500

    only run checkLoginStatus on mount

--- app/javascript/components/app.jsx
+++ app/javascript/components/app.jsx
@@ -42,7 +42,7 @@ const App = () => {
 
   useEffect(() => {
     checkLoginStatus();
-  })
+  }, []) // This only needs to run when <App /> is mounted. Changes will be caught by handleLogin and handleLogout and will remain in the session.
 
   return (
     <div>
