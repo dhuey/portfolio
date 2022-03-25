11cd6cced1755363852d33bc48ec6706bbd830e2
Dalton Huey <daltonjhuey@gmail.com>
2022-03-25 16:12:17 -0500

    create tech project controller and routes

---
+++ app/controllers/api/v1/tech_projects_controller.rb
@@ -0,0 +1,56 @@
+class Api::V1::TechProjectsController < ApplicationController
+  before_action :authenticate_user, except: [:index]
+  def index
+    @tech_projects = TechProject.all.order(created_at: :desc)
+
+    render json: @tech_projects
+  end
+
+  def new
+    @tech_project = tech_project.new
+
+    render json: @tech_project, status: :ok
+  end
+
+  def create
+    @tech_project = TechProject.new(tech_project_params)
+
+    if @tech_project.save
+      render json: @tech_project, status: :created
+    else
+      render json: @tech_project.errors, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @tech_project = TechProject.find(params[:id])
+
+    render json: @tech_project, status: :ok
+  end
+
+  def update
+    @tech_project = TechProject.find(params[:id])
+
+    if @tech_project.update(tech_project_params)
+      render json: @tech_project, status: :ok
+    else
+      render json: @tech_project.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @tech_project = TechProject.find(params[:id])
+
+    if @tech_project.destroy
+      render json: {message: "Successfully deleted tech project!"}, status: :ok
+    else
+      render json: @tech_project.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def video_params
+    params.require(:video).permit(:title, :youtube_link, :description)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :videos
+      resources :tech_projects
       resources :sessions, only: [:create ]
       delete :logout, to: "sessions#logout"
       get :logged_in, to: "sessions#logged_in"
