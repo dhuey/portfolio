3b2933a872b9850af58b1d011d9a4f3b7080d997
Dalton Huey <daltonjhuey@gmail.com>
2022-03-29 11:20:58 -0500

    add light and dark mode favicon

---
+++ app/javascript/modules/faviconSwitcher.js
@@ -0,0 +1,9 @@
+(()=> {
+        console.log("started");
+        const theme = window.matchMedia("(prefers-color-scheme: dark)");
+        if (theme.matches) {
+          document.getElementById("faviconTag").href = "dalton-huey-logo-dark.svg";
+        } else {
+          document.getElementById("faviconTag").href = "dalton-huey-logo.svg";
+        }
+      })();
