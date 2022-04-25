1b415cb60b08308659d893f70e1352fcf88e800d
Dalton Huey <daltonjhuey@gmail.com>
2022-04-25 17:30:34 -0500

    remove slashes form image src

--- app/javascript/components/Tech.jsx
+++ app/javascript/components/Tech.jsx
@@ -75,8 +75,8 @@ export const Tech = () => {
           <p>
             <strong>Skills & Technologies:</strong>
           </p>
-          <img src="/html5-logo.svg" alt="HTML" />
-          <img src="/css3-logo.svg" alt="CSS" />
+          <img src="html5-logo.svg" alt="HTML" />
+          <img src="css3-logo.svg" alt="CSS" />
           <img src="js-logo.svg" alt="JavaScript" />
           <img src="react-logo.svg" alt="React" />
           <img src="ruby-logo.svg" alt="Ruby" />
