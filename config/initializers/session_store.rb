f7d9361b94a56d6769785a4239fd7f1844e755d9
Dalton Huey <daltonjhuey@gmail.com>
2022-04-26 16:15:07 -0500

    update domain in session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_dalton_huey_portfolio", domain: "dalton-huey.herokuapp.com"
+  Rails.application.config.session_store :cookie_store, key: "_dalton_huey_portfolio", domain: "daltonhuey.com"
 else
   Rails.application.config.session_store :cookie_store, key: "_dalton_huey_portfolio"
 end
