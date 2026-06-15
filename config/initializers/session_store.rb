7a5ef67f77244eb84d8b1379e2f80222f23f619b
Dalton Huey <daltonjhuey@gmail.com>
2026-06-15 18:38:38 -0500

    update session_store domain for digital ocean

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_dalton_huey_portfolio", domain: "daltonhuey.com"
+  Rails.application.config.session_store :cookie_store, key: "_dalton_huey_portfolio", domain: "https://dev-portfolio-x4mlo.ondigitalocean.app/"
 else
   Rails.application.config.session_store :cookie_store, key: "_dalton_huey_portfolio"
 end
